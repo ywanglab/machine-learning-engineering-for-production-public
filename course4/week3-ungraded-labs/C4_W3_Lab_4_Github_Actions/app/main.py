f4bae7fb796a8c2c622c023893255f8c862a9742
ywanglab <ywang@DESKTOP-SBTU16T.localdomain>
2022-11-13 14:11:50 -0600

    Testing the CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -3,7 +3,7 @@ import numpy as np
 from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
-
+# adding this comment line
 
 
 app = FastAPI(title="Predicting Wine Class with batching")
